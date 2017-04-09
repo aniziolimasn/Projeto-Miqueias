b9e053bd816d562238c6fea40f6dc9fb20c09526
DESKTOP-K9TRT9P\Anizio Neto <aniziolimasn@gmail.com>
2017-04-09 11:34:03 -0300

    layout relativo

--- BankControl/app/src/main/java/com/example/anizioneto/bankcontrol/LoginActivity.java
+++ BankControl/app/src/main/java/com/example/anizioneto/bankcontrol/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.example.anizioneto.bankcontrol;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Context c = this;  // variavel context para navegar entre telas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //associando os valores passados pela tela
+        TextView txEmail = (TextView) findViewById(R.id.editTextEmail);
+        TextView txSenha = (TextView) findViewById(R.id.editTextSenha);
+        Button btnEntrar = (Button) findViewById(R.id.buttonEntrar);
+
+        //envento btnEntrar
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(c, MainActivity.class);
+
+
+
+
+                //navegar para outra tela
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 }
